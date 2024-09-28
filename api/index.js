cf7f0fcc46879e05541b6c92e0784ab30ed578d5
Kartik <narkhede.1@iitj.ac.in>
2024-09-28 17:46:46 +0530

    added database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
 const app=express();
+import  mongoose from 'mongoose';
+import 'dotenv/config'
 
+
+mongoose.connect(process.env.MONGO_URL).then(
+    ()=>{
+        console.log("Mongodb connected");
+    })
+    .catch(
+        (err)=>{
+            console.log(err);
+        }
+    )
 app.listen(3000,()=>{
     console.log("Server is running at port 3000");
 })
