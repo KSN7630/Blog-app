92588c9030be7f14e8f3fe873811a1cb3bba85eb
Kartik <narkhede.1@iitj.ac.in>
2024-09-30 17:02:06 +0530

    created test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+import User from "../models/user.model.js";
+
+
+export const test_get=(req,res)=>{
+    res.send("This is test request");
+}
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app=express();
 import  mongoose from 'mongoose';
 import 'dotenv/config'
-
+import userRoute from "./routes/user.route.js"
 
 mongoose.connect(process.env.MONGO_URL).then(
     ()=>{
@@ -16,3 +16,7 @@ mongoose.connect(process.env.MONGO_URL).then(
 app.listen(3000,()=>{
     console.log("Server is running at port 3000");
 })
+
+
+//use app.use as it is middleware
+app.use('/api/user',userRoute)
