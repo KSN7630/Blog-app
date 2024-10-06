3aa53fae75b44036073a2cea2af6a6e09f42feca
Kartik <narkhede.1@iitj.ac.in>
2024-10-06 20:46:39 +0530

    complete profile page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import { Button, TextInput } from 'flowbite-react';
 
 export default function DashProfile() {
-  const {currentUser} =useSelector(state=> state.user);
+  const { currentUser } = useSelector(state => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className="w-3 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-        <img src={currentUser.profilePicture} alt="user" className={`rounded-full w-full h-full object-cover border-8 border-[lightgray]` } />
+        <input type="file" accept='image/*' onChange={handleImageChange} />
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
         </div>
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
-        <TextInput type='password' id='password' placeholder='password'  />
+        <TextInput type='password' id='password' placeholder='password' />
         <Button type='submit' gradientDuoTone={'purpleToBlue'}>Update</Button>
       </form>
-      <div className='text-red-500 f;ex justify-between mt-5'>
+      <div className='text-red-500 flex justify-between mt-5'>
         <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign-Out</span>
       </div>
     </div>
   )
