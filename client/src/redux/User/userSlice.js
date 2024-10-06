4698f0b8f1e36848a827f6d9b6e416ab2caa91eb
Kartik <narkhede.1@iitj.ac.in>
2024-10-07 00:22:29 +0530

    solved error in updateUserSuccess

--- client/src/redux/User/userSlice.js
+++ client/src/redux/User/userSlice.js
@@ -24,12 +24,25 @@ export const userSlice = createSlice({
       signInFailure:(state,action)=>{
         state.loading=false;
         state.error=action.payload;
+      },
+      updateStart:(state)=>{
+        state.loading=true;
+        state.error=null;
+      },
+      updateSuccess:(state,action)=>{
+        state.loading=false;
+        state.error=null;
+        state.currentUser=action.payload
+      },
+      updateFailure:(state,action)=>{
+        state.loading=false;
+        state.error=action.payload;
       }
 
     },
   })
   
   // Action creators are generated for each case reducer function
-  export const { signInStart, signInSuccess, signInFailure } = userSlice.actions
+  export const { signInStart, signInSuccess, signInFailure ,updateStart,updateSuccess ,updateFailure} = userSlice.actions
   
   export default userSlice.reducer
