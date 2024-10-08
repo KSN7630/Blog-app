89099e75e40595d8ebf52b227e221187a81d533d
Kartik <narkhede.1@iitj.ac.in>
2024-10-09 00:49:22 +0530

    create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -26,3 +26,40 @@ export const create_post=async (req,res,next)=>{
 }
 
 
+export const get_post_get = async (req, res, next) => {  
+    try {
+        // Use parseInt instead of parsedInt
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortdirection = req.query.order === 'asc' ? 1 : -1;
+
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { postId: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } }, // Assuming you want to search the 'title'
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } }
+                ]
+            })
+        })
+        .sort({ updatedAt: sortdirection })
+        .skip(startIndex)
+        .limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+
+        const now = new Date();
+        const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+    } catch (err) {
+        next(err);
+    }
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from './../utils/verifyUser.js';
-import { create_post } from "../controllers/post.controller.js";
+import { create_post, get_post_get } from "../controllers/post.controller.js";
 
 const router=express.Router();
 
 
 
 router.post('/create',verifyToken,create_post)
+router.get('/getposts',get_post_get)
 
 
 export default router;
