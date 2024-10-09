e4947e0b5f3df4f09a00b82c7b65df24e15e0808
Kartik <narkhede.1@iitj.ac.in>
2024-10-09 19:59:39 +0530

    show users to admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar.jsx';
 import DashProfile from '../components/DashProfile.jsx';
 import DashPosts from '../components/DashPosts.jsx';
+import DashUsers from '../components/DashUsers.jsx';
 
 export default function Dashboard() {
   const loaction=useLocation();
@@ -23,6 +24,7 @@ export default function Dashboard() {
     
       {tab==='profile' && <DashProfile/>}
       {tab==='posts' && <DashPosts/>}
+      {tab==='users' && <DashUsers/>}
     </div>
   )
 }
